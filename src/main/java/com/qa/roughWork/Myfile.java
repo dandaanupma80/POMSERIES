006839177d6d191e3e215c92bbd42a005c8164d8
Danda <v788027@global.ual.com>
2022-04-13 11:03:02 -0500

    hello"

---
+++ src/main/java/com/qa/roughWork/Myfile.java
@@ -0,0 +1,12 @@
+package com.qa.roughWork;
+
+public class Myfile {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("this is rough wokr");
+
+	}
+
+}
